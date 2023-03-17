7723a555a354f4e8de5fdda1defaf1644feea462
Webdev_ment <bnelson@stu.ncu.edu.jm>
2023-03-17 16:33:00 -0500

    updated

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,35 @@
 <template>
   <div class="pa-5">
     <div class="mt-15">
+      <!--      based on who is log  in credentials one should be lead to the <admin-options> or <patient-options>-->
       <h1 >Login</h1>
       <br><br>
       <v-text-field label="Email Address"></v-text-field>
       <v-text-field label="Password"></v-text-field>
       <div class="d-flex justify-center">
-        <a to="/" href="#" @click="simulateLogin"> <v-btn color="primary">Login</v-btn></a>
-
+        <v-btn color="primary" href="/adminOptions">Login</v-btn>
       </div>
       <div class="d-flex justify-center mt-2">
-       <router-link to="/register">
-        <v-btn variant="text" color="primary" size="x-small">I don't have an account</v-btn>
-        </router-link>
+        <v-btn variant="text" color="primary" size="x-small" href="/register">I don't have an account</v-btn>
+
+        <!--       <router-link to="/register">-->
+<!--        <v-btn variant="text" color="primary" size="x-small" href="/register">I don't have an account</v-btn>-->
+<!--        </router-link>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import AdminOptions from "@/views/adminOptions.vue";
+import PatientOptions from "@/views/patientOptions.vue";
+
 export default {
   name: "Login",
+  components: {PatientOptions, AdminOptions},
   methods: {
     simulateLogin(){
-      localStorage.setItem("token", "bre");
+      localStorage.setItem("token", "chec");
       this.$router.push("/")
     }
   }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,13 +8,17 @@
       <v-text-field label="Last Name"></v-text-field>
       <v-text-field label="Email Address"></v-text-field>
       <v-text-field label="Password"></v-text-field>
+
       <div class="d-flex justify-center">
-        <router-link to="/"> <v-btn color="primary">Create Account</v-btn></router-link>
+        <v-btn color="primary" href="/register">Create Account</v-btn>
+<!--        <router-link to="/"> <v-btn color="primary" href="/register">Create Account</v-btn></router-link>-->
       </div>
       <div class="d-flex justify-center mt-2">
-        <router-link to="/login">
-          <v-btn variant="text" color="primary" size="x-small">I already have an account</v-btn>
-        </router-link>
+        <v-btn variant="text" color="primary" size="x-small" href="/login">I already have an account</v-btn>
+
+<!--        <router-link to="/login">-->
+<!--          <v-btn variant="text" color="primary" size="x-small">I already have an account</v-btn>-->
+<!--        </router-link>-->
       </div>
     </v-form>
 
